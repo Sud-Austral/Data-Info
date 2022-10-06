88dbf2d9f2b1f472a2910c5030af59428fbfe869
lmonsalve22 <lmonsalve22@gmail.com>
2022-10-06 17:42:44 -0300

    ahora si

--- automatizacion.py
+++ automatizacion.py
@@ -34,7 +34,10 @@ def main():
         detalle = pd.read_excel("comunas_origen.xlsx", sheet_name=j['nombre'])
         for n,k in detalle.iterrows():
             print(k["vista"],k["id_comuna"])
-            shutil.copy("base/base_vista.html", f"publicaciones2/{j['nombre']}/{k['vista_id']}")
+            try:
+                shutil.copy("base/base_vista.html", f"publicaciones2/{j['nombre']}/{k['vista_id']}")
+            except:
+                pass
             try:
                 
                 if(k["portada"] == 1):
