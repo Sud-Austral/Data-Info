9a32b58f045078803d360b88d9b6765fff8328cc
lmonsalve22 <lmonsalve22@gmail.com>
2022-10-07 16:42:32 -0300

    version 2

--- automatizacion.py
+++ automatizacion.py
@@ -33,22 +33,36 @@ def main():
 
         detalle = pd.read_excel("comunas_origen.xlsx", sheet_name=j['nombre'])
         htmlLi1 = ""
+        htmlLi2 = ""
         """
         htmlLi1 = '<li class="nav-item" role="presentation" >' + \
                    '<a href="./{}.html"> <button style="width: 100%!important; color:black" class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">'+ \
                    'Gestión Territorial'+ \
                    '</button></a></li>'
+
+                   <li class="nav-item" role="presentation" style="background:#C0CBD1;" >
+                    <a href="/Recursos_Infraestructura.html"><button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Recursos e Infraestructura</button></a> 
+                    </li>   
+
+        
         """
         for n,k in detalle.iterrows():
             print(k["vista"],k["id_comuna"])
             vista_id = k['vista_id']
             vista_nombre = k['nombre']
             try:
-                htmlAux = f"""<li class="nav-item" role="presentation" >
+                htmlAux1 = f"""<li class="nav-item" role="presentation" >
                             <a href="./{vista_id}.html"> <button style="width: 100%!important; color:black" class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">
                             {vista_nombre}
                             </button></a></li>"""
-                htmlLi1 = htmlLi1 + htmlAux
+                htmlLi1 = htmlLi1 + htmlAux1
+                print(htmlLi1)
+                htmlAux2 = f"""<li class="nav-item" role="presentation" style="background:#C0CBD1;" >
+                            <a href="/{vista_id}.html"><button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">
+                                {vista_nombre}
+                            </button>
+                            </a></li> """
+                htmlLi2 = htmlLi2 + htmlAux2
             except:
                 pass
             
